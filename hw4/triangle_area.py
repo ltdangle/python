70c1a14d3bdefc0a59a6a034c46a7f44ea3493df
Evgeniy Fedorov <mailbox@z01d.net>
2023-09-29 20:00:56 +0300

    added types

--- hw4/triangle_area.py
+++ hw4/triangle_area.py
@@ -1,4 +1,4 @@
-def triangle_area(x1, y1, x2, y2, x3, y3):
+def triangle_area(x1: float, y1:float, x2:float, y2:float, x3:float, y3:float)->float:
     return (1/2) * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
 
 
