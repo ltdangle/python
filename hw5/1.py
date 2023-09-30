4774b62e47a7f98565cbefc076cd9b57600087da
Evgeniy Fedorov <mailbox@z01d.net>
2023-09-30 11:29:43 +0300

    1.py

---
+++ hw5/1.py
@@ -0,0 +1,6 @@
+# False == not True 
+print(False == (not True))
+# True and False == True and False
+print( (True and False) == (True and False))
+# not True and "A" == "B"
+print( not (True and ("A" == "B")))
